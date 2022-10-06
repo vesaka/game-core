bcee23719f3c0b16e7c51976cbce2491d937cf0c
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2022-10-06 17:27:55 +0300

    add ucfirst and kebab string helpers

--- utils/string.js
+++ utils/string.js
@@ -1,3 +1,13 @@
-const ucfirst = () => {
-    
-}
+const ucfirst = (str) => {
+    return str.charAt(0).toUpperCase() + str.substr(1);
+};
+
+const kebab = (str) => {
+    return str.split('').map((letter, idx) => {
+        return letter.toUpperCase() === letter
+                ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
+                : letter;
+    }).join('');
+};
+
+export {kebab, ucfirst};
