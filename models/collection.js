135bd44163d63f4e99793d4c5432f83e4d4772d0
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2022-11-21 10:28:36 +0200

    new collection methods

--- models/collection.js
+++ models/collection.js
@@ -3,8 +3,8 @@ class Collection extends Container {
     constructor(options) {
 
         super(options);
-        
-        Object.assign(this, options);        
+
+        Object.assign(this, options);
 
         let filterMethod;
         for (let attribute in options) {
@@ -21,10 +21,18 @@ class Collection extends Container {
         return this.items;
     }
 
+    count(condition = null) {
+        if (typeof condition === 'function') {
+            return this.filter(condition).count();
+        }
+        
+        return this.items.length;
+    }
+
     avarage(key) {
         this.items.avarage(key);
     }
-    
+
     avg(key) {
         this.items.avarage(key);
     }
@@ -33,12 +41,26 @@ class Collection extends Container {
         this.items.push(item);
         return this;
     }
+    
+    get(key = 0) {
+        return this.items[key];
+    }
 
     remove(at) {
         this.items.splice(at, 1);
         return this;
     }
     
+    first(condition) {
+        for (let i in this.items) {
+            const stop = condition(this.items[i], i);
+            if (true === stop) {
+                return this.items[i];
+            }
+        }
+        return null;
+    }
+
     map(callback) {
         const result = [];
         for (let i in this.items) {
@@ -46,10 +68,13 @@ class Collection extends Container {
         }
         return result;
     }
-    
+
     each(callback) {
         for (let i in this.items) {
-            callback(this.items[i], i);
+            const stop = callback(this.items[i], i);
+            if (true === stop) {
+                break;
+            }
         }
         return this;
     }
@@ -61,7 +86,7 @@ class Collection extends Container {
             };
         }
 
-        return this.items.filter(condition);
+        return new Collection({items: this.items.filter(condition)});
     }
 
     merge() {
@@ -73,8 +98,45 @@ class Collection extends Container {
 
         return this;
     }
-    
-    
+
+    sum(arg) {
+        let sum = 0;
+        if ((typeof arg === 'string') || (typeof arg === 'number')) {
+            this.each(item => {
+                if (item[arg]) {
+                    sum += Number(item[arg]);
+                }
+            });
+
+        } else if (typeof arg === 'function') {
+            this.each((item, key) => {
+                sum += Number(arg(item, key));
+            });
+        }
+
+        return sum;
+    }
+
+    random() {
+        return this.items[Math.floor(Math.random() * this.count())]
+    }
+
+    shuffle() {
+        let currentIndex = this.items.length, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [this.items[currentIndex], this.items[randomIndex]] = [
+                this.items[randomIndex], this.items[currentIndex]];
+        }
+
+        return this;
+    }
+
+    slice(start = 0, end = 0) {
+        return new Collection(this.items.slice(start, end))
+    }
 }
 ;
 
