397d05f075e218ae79d1f7ed51229d878af94b51
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2022-11-21 10:09:32 +0200

    no matter pixi model

--- 2d/models/pixi-model.js
+++ 2d/models/pixi-model.js
@@ -1,8 +1,6 @@
 import Container from '$lib/game/core/container';
 
-import { Graphics, GraphicsGeometry, MeshMaterial, Point } from 'pixi.js';
-import { Body, Bodies, Vector } from 'matter-js';
-import { parse } from 'js-svg-path';
+import { Graphics, Point } from 'pixi.js';
 const HALF_PI = Math.PI / 2;
 
 const ARRAY_ATTRIBUTES = ['axes', 'vertices', 'parts', ];
@@ -23,99 +21,43 @@ class Model extends Container {
         if (!this.position) {
             this.position = new Point(0, 0);
         }
-
+        
         this.model = this.createModel();
-        this.body = this.createBody();
-        //Body.setCentre(this.body, true);
         this.$name = this.constructor.name.toLowerCase();
         return this;
     }
-
-    set(name, value) {
-        this[name] = value;
-        this.model[name] = value;
-        this.body[name] = value;
-        return this;
-    }
-
+    
     createModel() {
         return new Graphics();
     }
 
-    createBody() {
-        return Bodies.rectangle();
-    }
-
-    createVerticesFromSvgPath(path) {
-        const {curveshapes} = parse(path);
-        const vertices = [];
-
-        for (let i in curveshapes) {
-            for (let j in curveshapes[i].points) {
-                vertices.push(
-                    Vector.create(
-                        curveshapes[i].points[j].main.x,
-                        curveshapes[i].points[j].main.y
-                    )
-                );
-            }
-        }
-        
-        return vertices;
-
-    }
-
     setPosition(x = 0, y = 0) {
-        Body.setPosition(this.body, {x, y});
         this.model.position.set(x, y);
         return this;
     }
 
     setX(value) {
-        Body.setPosition(this.body, { x: value, y: this.body.position.y });
-        this.model.position.x = this.body.position.x;
+        this.model.position.x = value;
         return this;
     }
 
     setY(value) {
-        Body.setPosition(this.body, { x: this.body.position.x, y: value });
-        this.model.position.y = this.body.position.y;
+        this.model.position.y = value;
         return this;
     }
 
     increaseX(value) {
-        Body.setPosition(this.body, { x: this.body.position.x + value, y: this.body.position.y });
-        this.model.position.x += this.body.position.x;
+        this.model.position.x += value;
         return this;
     }
 
     increaseY(value) {
-        Body.setPosition(this.body, { x: this.body.position.x, y: this.body.position.y + value });
-        this.model.position.y = this.body.position.y;
+        this.model.position.y = value;
         return this;
     }
 
     rotate(angle) {
-        Body.setAngle(this.body, angle);
         this.model.rotation = angle;
-        return this;
-    }
-
-    update() {
-        this.model.rotation = this.body.angle;
-        this.updatePosition();
-        return this;
-    }
-
-    updatePosition() {
-        this.model.position.x = this.body.position.x;
-        this.model.position.y = this.body.position.y;
-
-        return this;
-    }
-
-    updateRotation() {
-        this.model.angle = this.body.angle;
         return this;
     }
 
@@ -140,25 +82,20 @@ class Model extends Container {
         return val;
     }
 
-    filter_matter(body = {}) {
-        const matter = Object.assign({}, body);
-        if (typeof matter === 'object') {
-            for (let key in matter) {
-                if (ARRAY_ATTRIBUTES.indexOf(key) > -1) {
-                    continue;
-                }
-
-                if (Array.isArray(matter[key]) && matter[key].length === 2) {
-                    matter[key] = Math.between(matter[key][0], matter[key][1]);
-                }
-            }
-        }
-
-        return matter;
-    }
-
     filter_style(style) {
         return this.filterObject(style);
+    }
+    
+    hide() {
+        this.model.visible = false;
+    }
+    
+    show() {
+        this.model.visible = true;
+    }
+    
+    toggle() {
+        this.model.visible = !this.model.visible;
     }
 
 }
