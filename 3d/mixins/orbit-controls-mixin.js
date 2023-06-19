3d79b2dbc21adc86afef9eb4a22edd196f26263f
Veselin <vesakabgr@gmail.com>
2023-06-19 04:43:01 +0300

    use canvas

--- 3d/mixins/orbit-controls-mixin.js
+++ 3d/mixins/orbit-controls-mixin.js
@@ -1,9 +1,9 @@
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 export const useOrbitControls = (instance) => {
-    const {camera, canvas, renderer, scene} = instance;
+    const {camera, canvas} = instance;
 
-    instance.controls = new OrbitControls(camera, canvas.parentNode);
+    instance.controls = new OrbitControls(camera, canvas);
     instance.controls.enableDamping = true;
     instance.controls.update();
 };
